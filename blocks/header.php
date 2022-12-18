28ad5a4fbbf4a19081b5e953a4c8c9b1fc6921d9
arahisss <maindelay@mail.ru>
2022-12-18 15:33:13 +0300

    add lab5

--- blocks/header.php
+++ blocks/header.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style.css">
-    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="style.css">
+    
     <?php 
-    $title = "Аверкова Карина 211-361 ЛР № 3";
+    $title = "Аверкова Карина 211-361 ЛР № 5";
     ?>
     <title><?php echo($title);?></title>
     
@@ -33,25 +33,13 @@
     <section class="section">
         <div class="container">
             <header class="header">
-                <a class="header__logo" href="index.php">MyPage</a>
+                <a class="header__logo" href="index.php">Lab5</a>
 
                 <nav class="nav">
-                    <a href="<?php
-                        $name='Главная';
-                        $link='index.php';
-                        $is_button = false;
-                        
-                        echo $link;
-                    ?>"<?php
-                        if ($is_button)
-                            echo ' class="login_button"';
-                        else
-                            echo ' class="nav__link"';
-                    ?>><?php echo $name;?></a>
-
-                    <!-- <a class="nav__link" href="index.php">Главная</a>
+                    
+                    <a class="nav__link" href="index.php">Главная</a>
                     <a class="nav__link" href="#">Контакты</a>
-                    <a class="nav__link" href="feedback.php">Обратная связь</a> -->
+                    <a class="nav__link" href="#">Обратная связь</a>
                     <a class="login-button" href="auth.php">Войти</a>
                     
                 </nav>
--- index.php
+++ index.php
@@ -1,26 +1,23 @@
 <?php require("blocks/header.php"); ?>
     
     <main>
-        <h1>Hello</h1>
+        <h1>Images from database</h1>
+      
         <?php
-            define('DB_HOST', 'std-1971.ist.mospolytech.ru'); //Адрес
-            define('DB_USER', 'std'); //Имя пользователя
-            define('DB_PASSWORD', 'root1234'); //Пароль
-            define('DB_NAME', 'std_1971_lab5'); //Имя БД
-            $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-            echo $mysqli->host_info;
-         
-
-            $result = mysqli_query($mysql, "SELECT * FROM `images`");
-            echo $result;
+            include "db.php";
+            $result = mysqli_query($connect, "SELECT * FROM `images`");
         ?>
 
         <div class="content">
-            <img src="img/img_1.JPG" alt="1">
-            <img src="img/img_2.JPG" alt="2">
-            <img src="img/back.jpg" alt="3">
+            <?php while($img = mysqli_fetch_assoc($result)) {
+                ?>
+                
+                <img class="img_db" src="img/<?=$img["name"];?>" alt="<?=$img["title"];?>" title="<?=$img["title"];?>">
+            <?php
+            };
+            ?>
         </div>
         
     </main>
 
-<?php require("blocks/footer.php") ?>
+<?php require("blocks/footer.php"); ?>
